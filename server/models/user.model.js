580e7d80835735b96830bf62654f6877274811ce
Piyushrathoree <meonpath008@gmail.com>
2025-01-29 20:54:25 +0000

    user model done

---
+++ server/models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "password is required"],
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.statics.hashPass=async function(password){
+    return await bcrypt.hash(password , 10);
+}
+
+userSchema.methods.generateToken = function (){
+    return jwt.sign(
+        { _id: this._id },
+        process.env.JWT_SECRET,
+        {expiresIn:'24h'}
+    );                                                                                                                                                                                                                                                                                                                                                                                
+}
+
+userSchema.methods.isPasswordCorrect = async function(){
+    return await bcrypt.compare(password ,this.password)
+}
+
+export const User = mongoose.model("User", userSchema);
