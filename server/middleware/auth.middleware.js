89f5783d0650c054f436870f56240cfc24b7d756
Piyushrathoree <meonpath008@gmail.com>
2025-01-30 15:14:25 +0530

    completed middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+const AuthenticateUser = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    return next();
+};
+
+export default AuthenticateUser;
