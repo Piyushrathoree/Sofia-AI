9d70cf6c64d8fda7fbb71bd59f7b0bfd49391855
Piyushrathoree <meonpath008@gmail.com>
2025-02-01 02:38:22 +0530

    feat: add console log for MongoDB connection and update chat route to '/Ai'

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 function connectDB() {
     const URL = `${process.env.MONGODB_URI}/${process.env.DB_NAME}`;
+    console.log("Connecting to MongoDB...");
     return mongoose.connect(URL);  // Return the promise
 }
 
--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -3,7 +3,7 @@ import { assistant } from '../controllers/chat.controller.js';
 import AuthenticateUser from '../middleware/auth.middleware.js';
 const router = express.Router();
 
-router.post('/' , AuthenticateUser , assistant)
+router.post('/Ai' , AuthenticateUser , assistant)
 
 
 export default router;
