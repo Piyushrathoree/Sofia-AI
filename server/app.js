ebe206e50f28658ec7449e938e1ba0c22f2545e1
Piyushrathoree <meonpath008@gmail.com>
2025-01-31 06:17:10 +0530

    feat: configure CORS to allow specific origin and credentials

--- server/app.js
+++ server/app.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Replace with frontend URL
+      credentials: true, // Allow cookies & auth headers
+    })
+  );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
