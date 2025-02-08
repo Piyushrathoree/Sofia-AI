d3b44160e972f9a143e6e436a16428fd07e55dc4
Piyushrathoree <meonpath008@gmail.com>
2025-02-09 00:19:49 +0530

    feat: add update profile and change password routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserProfile, loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
+import { getUserProfile, loginUser, logoutUser, registerUser, updatePassword, updateProfile } from '../controllers/user.controller.js';
 import AuthenticateUser from '../middleware/auth.middleware.js';
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.post("/register" , registerUser)
 router.post("/login" , loginUser)
 router.post("/logout" , logoutUser)
 router.get ('/profile', AuthenticateUser , getUserProfile )
+router.put('/update', AuthenticateUser, updateProfile)
+router.put('/change-password', AuthenticateUser, updatePassword)
 
 export default router;
